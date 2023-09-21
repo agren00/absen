c889df5347927d2c103f1056fd09293bb4a40a2e
mercyiren <mercyirene080@gmail.com>
2023-09-21 15:45:55 +0700

    merci yamg membuat halaman login ini

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,52 @@
-// terserah tulis apa yaaa
-// semangat yaaa hari ini
-// terimakasih karena telah membantu akuu
-// heheeh bersyukur selalu yaaa jangan mengeluh karena mengeluh itu gak baikkk wkwk
-// cobahhh yaaaaa rubah dan buat branches1
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(project());
+}
+
+class project extends StatelessWidget {
+  const project({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Login(),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(child: Text("LOGIN")),
+              SizedBox(height: 20),
+              Text("Email"),
+              TextField(),
+              Text("Password"),
+              TextField(),
+              SizedBox(height: 20),
+              ElevatedButton(onPressed: () {}, child: Text("Masuk"))
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
+//  ini halaman login yang di buat mercy //
